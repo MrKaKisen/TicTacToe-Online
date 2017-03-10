bc2b4e23627d20945ed2c11fa786647e126fd713
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-10 19:45:28 +0100

    removed IP

--- client.py
+++ client.py
@@ -54,7 +54,7 @@ pygame.init()
 display_width = 1280
 display_height = 720
 version = "a1.2-dev"
-mainServerAddr = ("http://us1-orfeus.tictactoe.mrkakisen.net:2016/")
+mainServerAddr = ("removed - please use the compiled client")
 # sq pos
 # the cero
 sqxPos = []
